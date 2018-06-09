147fdd00edcdc3bce2e58c0a7b85bfad17e8aba6
lipengyuan <495832952@qq.com>
2018-06-09 18:14:47 +0800

    delete two file

--- exercise/ex35.py
+++ exercise/ex35.py
@@ -25,6 +25,9 @@ def bear_room():
         if next == "take honey":
             dead("The bear looks at you then slaps your face off.")
         elif next == "taunt bear" and not bear_moved:
+            print("The bear has moved from the door. You can go through it now.")
+            bear_moved = True
+        elif next == "taunt bear" and bear_moved:
             dead("The bear gets pissed off and chews your leg off.")
         elif next == "open door" and bear_moved:
             gold_room()
